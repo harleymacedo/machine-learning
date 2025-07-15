c93bfedd979a1652b2d36b89f6ddcf6bd0ac95f8
harleymacedo <harley.ip@gmail.com>
2025-07-15 16:27:45 -0300

    Iniciado exemplo de modelo linear

--- exemplo1/exemplo1.py
+++ exemplo1/exemplo1.py
@@ -1,1 +1,12 @@
 print('Exemplo 1 - Python')
+
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model 
+
+# Carrega os dados
+oecd_bli = pd.read_csv('oecd_bli_2015.csv', thousands=',')
+gdp_per_capita = pd.read_csv('gdp_per_capita.csv', thousands=',', delimiter='\t', encoding='latin1', na_values='n/a')
+
+# Prepara os dados 
