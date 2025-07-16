b2ee639ac2499ff5c7e7fc71d7bbd3374b758c09
harleymacedo <harley.ip@gmail.com>
2025-07-16 15:37:03 -0300

    Exemplo finalizado, mas precisa do ambiente de teste

--- exemplo1/exemplo1.py
+++ exemplo1/exemplo1.py
@@ -10,3 +10,20 @@ oecd_bli = pd.read_csv('oecd_bli_2015.csv', thousands=',')
 gdp_per_capita = pd.read_csv('gdp_per_capita.csv', thousands=',', delimiter='\t', encoding='latin1', na_values='n/a')
 
 # Prepara os dados 
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+x = np.c_[country_stats['GDP per capita']]
+y = np.c_[country_stats['Life satisfaction']]
+
+# Visualiza os dados
+country_stats.plot(kind='scatter', x='GDP per capita', y='Life satisfaction')
+plt.show()
+
+# Seleciona um modelo linear
+model = sklearn.linear_model.LinearRegression()
+
+# Treina o modelo
+model.fit(x, y)
+
+# Efetua uma predição para o Chipre
+X_new = [[22587]]  # GDP per capita do Chipre
+print(model.predict(X_new))  # Entradas [[5.96242338]]
