a0cc1ffac28630ab3f79e54ec1548ff38c610fdb
harleymacedo <harley.ip@gmail.com>
2025-07-18 22:26:50 -0300

    Exemplo de array

--- exemplo2/exemplo2.py
+++ exemplo2/exemplo2.py
@@ -2,3 +2,6 @@ nome = 'Francisco'
 idade = 43
 print(f'Nome: {nome}, Idade: {idade}')
 
+lista_carros = ['Gol', 'Uno', 'HB20', 'Ka']
+print(f'Conteúdo da lista: {lista_carros}')
+print(f'O primeiro elemento da lista é: {lista_carros[0]}') 
