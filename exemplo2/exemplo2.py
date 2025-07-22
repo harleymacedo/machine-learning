1915bc97fe61dc414c961bc37e4c4fcfd361785d
harleymacedo <harley.ip@gmail.com>
2025-07-21 23:58:13 -0300

    Exemplo de array melhorado

--- exemplo2/exemplo2.py
+++ exemplo2/exemplo2.py
@@ -5,3 +5,5 @@ print(f'Nome: {nome}, Idade: {idade}')
 lista_carros = ['Gol', 'Uno', 'HB20', 'Ka']
 print(f'Conteúdo da lista: {lista_carros}')
 print(f'O primeiro elemento da lista é: {lista_carros[0]}') 
+lista_carros.append('Civic')
+print(f'Lista após adicionar um carro: {lista_carros}')
