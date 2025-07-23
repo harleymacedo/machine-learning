8cec2b39464af1e9aeaa303e05781d9309c46183
harleymacedo <harley.ip@gmail.com>
2025-07-23 11:20:48 -0300

    Extratificação de dados

--- exemplo5/exemplo5.py
+++ exemplo5/exemplo5.py
@@ -6,3 +6,10 @@ def split_train_test(data, test_ratio):
     test_indices = shuffled_indices[:test_set_size]
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing['income_cat']):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
