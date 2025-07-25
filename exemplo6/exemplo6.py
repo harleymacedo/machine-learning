04fce91ed7a25b338f9320fef7dd4645e133df65
harleymacedo <harley.ip@gmail.com>
2025-07-24 23:23:03 -0300

    Plotagem de gráficos sobre preços de imóveis

--- exemplo6/exemplo6.py
+++ exemplo6/exemplo6.py
@@ -1,2 +1,8 @@
 corr_matrix = housing.corr()
 corr_matrix['median_house_value'].sort_values(ascending=False)
+
+from pandas.plotting import scartter_matrix
+
+attributes = ['median_house_value', 'median_income', 'total_rooms', 'housing_median_age']
+
+scartter_matrix(housing[attributes], figsize=(12, 8))
