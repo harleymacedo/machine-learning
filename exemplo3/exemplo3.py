22e1b0ce7bfa431a772b4d6ccd9c63338e5844f8
harleymacedo <harley.ip@gmail.com>
2025-08-11 08:53:31 -0300

    Perfecting the model

--- exemplo3/exemplo3.py
+++ exemplo3/exemplo3.py
@@ -79,3 +79,14 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
     from sklearn.tree import DecisionTreeRegressor
     tree_reg = DecisionTreeRegressor()
     tree_reg.fit = (housing_prepared, housing_labels)
+
+    from sklearn.model_selection import GridSearchCV
+    param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+    ]
+    forest_reg = RandomForestRegressor()
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                               scoring='neg_mean_squared_error',
+                               return_train_score=True)
+    grid_search.fit(housing_prepared, housing_labels)
